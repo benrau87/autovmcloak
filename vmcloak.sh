ddfd498787e6fd9a0e731daaa9010d41d2a05516
benrau87 <benrau87@users.noreply.github.com>
2017-02-21 09:50:33 -0600

    Update vmcloak.sh

--- vmcloak.sh
+++ vmcloak.sh
@@ -111,6 +111,8 @@ echo -e "${YELLOW}What is the name for this machine?${NC}"
 read name
 echo -e "${YELLOW}What is the key?${NC}"
 read key
+echo -e "${YELLOW}What is the distro? (winxp, win7x86, win7x64, win81x86, win81x64, win10x86, win10x64)${NC}"
+read distro
 
 
 echo -e "${YELLOW}###################################${NC}"
@@ -118,7 +120,7 @@ echo -e "${YELLOW}This process will take some time, you should get a sandwich, o
 echo
 sleep 5
 #--hwvirt
-vmcloak init --vm-visible --ramsize 2048 –cpus 2 -ip $ipaddress --serial-key $key --cuckoo /etc/cuckoo-modified/utils/machine.py --iso-mount /mnt/windows_ISOs/ $name &>> $logfile
+vmcloak init --$distro --vm-visible --ramsize 2048 –cpus 2 -ip $ipaddress --serial-key $key --cuckoo /etc/cuckoo-modified/utils/machine.py --iso-mount /mnt/windows_ISOs/ $name &>> $logfile
 error_check 'Created VMs'
 vmcloak install $name adobe9 wic pillow dotnet40 java7 removetooltips windows_cleanup chrome firefox_41 &>> $logfile
 error_check 'Installed adobe9 wic pillow dotnet40 java7 removetooltips windows_cleanup chrome firefox_41 on VMs'

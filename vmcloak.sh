7a093627ce4677e13e7558e77ff8700a9a5fab2f
benrau87 <benrau87@users.noreply.github.com>
2017-02-21 11:51:13 -0600

    Update vmcloak.sh

--- vmcloak.sh
+++ vmcloak.sh
@@ -125,7 +125,7 @@ echo -e "${YELLOW}This process will take some time, you should get a sandwich, o
 echo
 sleep 5
 #--hwvirt
-vmcloak init --$distro --vm-visible --ramsize $ram –-cpus $cpu --ip $ipaddress --serial-key $key --iso-mount /mnt/windows_ISOs/ $name &>> $logfile
+vmcloak init --$distro --vm-visible --ramsize $ram --cpus $cpu --ip $ipaddress --serial-key $key --iso-mount /mnt/windows_ISOs/ $name &>> $logfile
 error_check 'Created VMs'
 vmcloak install $name adobe9 wic pillow dotnet40 java7 removetooltips windows_cleanup chrome firefox_41 &>> $logfile
 error_check 'Installed adobe9 wic pillow dotnet40 java7 removetooltips windows_cleanup chrome firefox_41 on VMs'
